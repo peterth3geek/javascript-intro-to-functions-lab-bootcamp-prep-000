b311b952d39bdaf07579ab7bd6cf20d548b20524
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-13 03:22:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string){
   if (string = string.toLowerCase) {
     return "I can't hear you!"
-  }
-  else if (string = string.toUpperCase) {
+  }}
+function sayHiToGrandma(string) {
+  if (string=string.toUpperCase){
     return "YES INDEED!"
   }
-  else {
-    return "I love you, too."
-  }
 
-  }
+}
