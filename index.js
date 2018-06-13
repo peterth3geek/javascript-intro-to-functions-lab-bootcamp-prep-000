505481a482b0952610895fb795f14008af5f33b5
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-13 03:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 
-var string = string
-
 function sayHiToGrandma(string){
   if (string === string.toLowerCase) {
     return "I can't hear you!"
   }}
 function sayHiToGrandma(string) {
-  if (string===string.toUpperCase){
+  if (string === string.toUpperCase){
     return "YES INDEED!"
   }
 
