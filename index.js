3df7b98ef5f4e0374acfd05b23c28cb9c7b66f37
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-13 03:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function sayHiToGrandma(string) {
 
   else if (string === string.toUpperCase()) {
     return "YES INDEED!"
+  }
+  else if (string === 'I love you, Grandma.') {
+    return "I love you, too."
 
+  }
   }
 
 }
